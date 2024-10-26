c08cab9362de313346be1f79dbe03557fcba692e
Bruce Munoz <107725994+Bmuno23@users.noreply.github.com>
2024-10-26 11:41:56 -0400

    Completed left side of Page Header, Added comments

--- YouTube-Page/src/components/Button.tsx
+++ YouTube-Page/src/components/Button.tsx
@@ -1,14 +1,28 @@
 import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
+//create new const which uses cva (which is a function which creates CSS class names based on diff variant and states)
+/*
+hover: bg-secondary-hover -> applies when button is hovered
+transition-colors -> adds a transition effect to color changes
+*/
 const buttonStyles = cva(["hover: bg-secondary-hover", "transition-colors"], {
+  //create "variants"(props) - basically new class names, when added to "Button" they apply which ever properties are attached
+  // --> usually done to get rid of repetitive class names/styles
+  //ex: <Button variant: "ghost" ></Button> --> applies "ghost properties from "variant" (not variants)
   variants: {
-    variant:{
-        default:["bg-secondary", "hover:bg-secondary-hover"],
-            ghost:["hover: bg-grey-100"],
-    }, 
+    //A sub storage for different variants -> variant = ""
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover: bg-grey-100"],
+    },
+
+    //A sub storage for different sizes -> size = ""
     size: {
       default: ["rounded", "p-2"],
+
+      //creates round icon
       icon: [
         "rounded-full",
         "w-10",
@@ -19,14 +33,32 @@ const buttonStyles = cva(["hover: bg-secondary-hover", "transition-colors"], {
       ],
     },
   },
+
+  //default values for variant and size sub storages
   defaultVariants: {
     variant: "default",
     size: "default",
-  }
+  },
 });
 
-type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">
+//Define "ButtonProps" type which extends VariantProps based on "buttonStyles" and def props of "Button" element
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
 
-export function Button({variant,size, ...props}: ButtonProps) {
-  return <button {...props} className={buttonStyles({variant, size})}></button>;
+//export "Button" component (<Button></Button>) that accepts "variant", "size", reg tailwind className and other button props
+export function Button({ variant, size, className, ...props }: ButtonProps) {
+  //return a button element with spreads(...props) reaming props and merge class names
+  return (
+    <button
+      //takes in all remaining properties that weren't destructured in function parameter  of (variant, size, className)
+      //and adds (if applied) to button element ex: wanting to add "onClick" ...props would take it in
+      {...props}
+
+      //use twMerge to make sure styles don't overwrite one another
+      /*
+      buttonStyles({variant, size}) --> applies classes from "buttonStyles"
+      className --> applies any other classes (regular like bg-color)
+      */ 
+      className={twMerge(buttonStyles({ variant, size }), className)}
+    ></button>
+  );
 }
--- YouTube-Page/src/layouts/PageHeader.tsx
+++ YouTube-Page/src/layouts/PageHeader.tsx
@@ -1,20 +1,33 @@
+import { Menu } from "lucide-react";
+import logo from "../assets/Screenshot 2024-10-26 at 10.02.31 AM.png";
+import { Button } from "../components/Button";
 
+export function PageHeader() {
+  return (
+    // press ctrl / for comment
 
-import { Menu } from "lucide-react"
-import logo from "../assets/Screenshot 2024-10-26 at 10.02.31 AM.png"
-import { Button } from "../components/Button"
+        // Left side of page header ------
+    <div /*main container*/ className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+      <div /*button container*/ className="flex gap-4 items-center flex-shrink-0">
 
-export function PageHeader(){
-    return <div className="flex gap-10 lg:gap-20 justify-between">
-        <div className="flex gap-4 items-center flex-shrink-0">
-            <Button>
-                <Menu></Menu>
-            </Button>
-            <a href="/">
-                <img src={logo} alt="logo" className="h-6" />
-            </a>
-        </div>
-        <div></div>
-        <div></div>
+        {/*type "Button" for drop down menu - brings the "Button" from tsx file - has some classes which are specified within tsx file  */}
+        <Button variant="ghost" size="icon">
+
+        {/* Menu icon from lucide-react library */}
+          <Menu></Menu>
+        </Button>
+
+        {/* img of youtube logo - has link to nowhere bc just front end proj */}
+        <a href="/">
+          <img src={logo} alt="logo" className="h-12" />
+        </a>
+      </div>
+
+      {/* Middle of page header ------*/}
+      <div></div>
+
+      {/* Right side of page header ------*/}
+      <div></div>
     </div>
+  );
 }
